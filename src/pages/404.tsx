57dfb73874d05972fc1bf218d567b109a59ed53b
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-09-23 10:44:42 +0200

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,21 @@
+import {buttonVariants} from "@/components/ui/button";
+import {ArrowRight} from "lucide-react";
+import Link from "next/link";
+
+export default function Page() {
+	return (
+		<>
+			<div className="flex flex-col items-center justify-center min-h-screen py-2 space-y-4">
+				<h1 className="text-6xl font-bold">404</h1>
+				<p className="text-2xl">PAGE NOT FOUND</p>
+				<Link
+					href="/dashboard"
+					className={buttonVariants({variant: "outline"})}
+				>
+					<ArrowRight className="mr-2 h-4 w-4"/>
+					<span>Return</span>
+				</Link>
+			</div>
+		</>
+	);
+}
