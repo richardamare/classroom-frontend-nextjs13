546de1a2c0b181bdf4a5ff276c67f21811366c84
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-09-23 16:53:34 +0200

    feat: add logout button

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,9 @@
-import {Button} from "@/components/ui/button";
-import {cn} from "@/lib/utils";
+import {Button, buttonVariants} from "@/components/ui/button";
 import {useRouter} from "next/router";
 import {sidebarItems} from "@/lib/config";
+import Link from "next/link";
+import {LogOut} from "lucide-react";
+import {cn} from "@/lib/utils";
 
 interface SidebarProps {
 	className?: string;
@@ -16,8 +18,8 @@ export default function Sidebar({className}: SidebarProps) {
 	}
 
 	return (
-		<div className={cn("pb-12", className)}>
-			<div className="space-y-4 py-4">
+		<div className={className}>
+			<div className="space-y-4 py-4 flex flex-col justify-between h-full">
 				<div className="px-4 py-2 space-y-6">
 					<div className="flex items-center justify-center h-20">
 						<h1>LOGO</h1>
@@ -36,6 +38,20 @@ export default function Sidebar({className}: SidebarProps) {
 								</Button>
 							</>
 						))}
+					</div>
+				</div>
+				<div className="border-t">
+					<div className="px-4 py-2 space-y-6">
+						<Link
+							href="/auth/logout"
+							className={cn(
+								buttonVariants({variant: "link"}),
+								"hover:no-underline text-muted-foreground"
+							)}
+						>
+							<LogOut className="mr-2 w-4 h-4"/>
+							<span>Odhlásit se</span>
+						</Link>
 					</div>
 				</div>
 			</div>
